b65cdf87d66250a34536d21403a754e1d4079283
sudharshanreddy007 <31057445+sudharshanreddy007@users.noreply.github.com>
2017-08-30 12:13:10 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,23 @@
+
+public class Helloworld {//public is a Java keyword which declares a member's access as public. 
+
+	public static void main(String[] args) {//public : it is a access specifier that means it
+		//will be accessed by publically. 
+		//static : it is access modifier that means when the java program is load then it
+		//will create the space in memory automatically. 
+		//void : The keyword void simply tells the compiler that main ( ) does not return a value.  
+			//	main() : it is a method or a function name.
+		//String args [ ] declares a parameter named args, which is an array of
+		//instances of the class String. In this case, 
+		//args receives any command-line arguments present when the program is executed.
+		System.out.println("Hello World");//System is a predefined class that
+		//provides access to the system which is introduced inside java lang.package. 
+		//System class is a default class. 
+		//Out is the static attribute which is calling print method.
+		//Out is a static attribute inside a system class
+		//.Println ( ) displays the string which is passed to it. 
+		//System.out.println displays its argument in the command window followed by 
+		//a new-line character to position the output cursor to the beginning of the next line. 
+	}
+
+}
